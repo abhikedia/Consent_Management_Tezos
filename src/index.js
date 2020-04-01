6d57d6367c47383984d6d2e936d68debb2483276
Abhishek Kedia <iamabhikedia74@gmail.com>
2020-04-02 04:38:16 +0530

    Partial Registration Form Complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Register from './Registration/register';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Register/>
   </React.StrictMode>,
   document.getElementById('root')
 );
